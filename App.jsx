bdf64bca90c3a005d023f5141176eb1bea9ec8bb
333shridharhegde <77891412+333shridharhegde@users.noreply.github.com>
2021-06-26 18:56:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Card from "./Info";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This is an online bootcamp that 
+        teaches javascipt from scratch</p>
+      <p>7 days bootcamp</p>
+      <p>No pre-requisites required</p>
+    </div>
+  );
+}
+
+export default Info;
